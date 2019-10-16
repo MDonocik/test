c2e189e47b0b0cc804f1a22ac4669dcd66215541
MDonocik <kaszub.jks@gmail.com>
2019-10-16 08:40:05 +0200

    Add new hold comity project

---
+++ Natek to lama/Assets/scripts/helloword.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloword : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+ 
+   void Start()
+    {
+        int min = 1;
+        int max = 1000;
+        print("Witaj w Liczbowym Czarodzieju");
+        print("Wybierz liczbę z przedizału od 1 do 1000");
+        print("maksymalna jaką możesz wybraźć to: " + max);
+        print("minimalna jaką możesz wybraźć to: " + min);
+        print("Czy twoja liczba jest mniejsza lub większa niż 500")
+        print("naciśnij górną strzałkę aby odpowiedziweć, że większa");
+        print("naciśnij dół strzałkę aby odpowiedziweć, że mniejsza");
+        print("naciśnij enter aby potwierdzić, że koputer zgadłliczbę");
+
+        if (inpyt.GetKeyDown)
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
